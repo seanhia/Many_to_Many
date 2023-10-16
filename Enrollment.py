f21905beb34d89dfa529bbc46f43a8bace893b7d
lucas <lucas.gonzalez@student.csulb.edu>
2023-10-16 01:12:47 -0700

    so, might have to ask prof, but this fixed the InvalidRequestError

--- Enrollment.py
+++ Enrollment.py
@@ -7,13 +7,25 @@ class Enrollment(Base):
     __tablename__ = "enrollments"
 
     #no attributes here except for discriminator
-    student: Mapped["Student"] = relationship(back_populates="sections")
-    section: Mapped["Section"] = relationship(back_populates="students")
+    student: Mapped["Student"] = relationship(back_populates="enrollments")
+    section: Mapped["Section"] = relationship(back_populates="enrollments")
     #surrogate i think? based off roadmap, coming from student & section
     studentID: Mapped[int] = mapped_column('student_id', ForeignKey("students.student_id"),
                                            primary_key=True)
     sectionID: Mapped[int] = mapped_column('section_id', ForeignKey("sections.section_id"),
                                            primary_key=True)
+    '''departmentAbbreviation: Mapped[str] = mapped_column('department_abbreviation',
+                                                        ForeignKey("sections.department_abbreviation"),
+                                                        primary_key=True)
+    courseNumber: Mapped[str] = mapped_column('course_number', ForeignKey("sections.course_number"),
+                                              primary_key=True)
+    sectionYear: Mapped[int] = mapped_column('section_year', ForeignKey("sections.section_year"),
+                                             primary_key=True)
+    semester: Mapped[str] = mapped_column('semester', ForeignKey("sections.semester"),
+                                          primary_key=True)
+
+    __table_args__ = (UniqueConstraint('department_abbreviation', 'course_number', 'section_year',
+                                       #'semester', 'student_id', name="enrollments_uk_01"),)'''
 
     def __init__(self, student, section):
         self.student = student
